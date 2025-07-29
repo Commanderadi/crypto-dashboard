96ba225a199579e68f46806fa22b7f5dab805d06
Commanderadi <adityasingh3499@gmail.com>
2025-07-29 19:01:37 +0530

    Update frontend to use live Render backend URL

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const api = axios.create({ baseURL: "http://localhost:5000/api" });
+export const api = axios.create({ baseURL: "https://intelicrypto.onrender.com/api" });
 
 export function getAuthHeaders() {
   const token = localStorage.getItem("token");
